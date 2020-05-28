f11dfefe02151d1417e0c5b53dea334029874291
LacyRayAnne <LacyRayAnne@gmail.com>
2020-05-28 14:54:55 -0400

    Finished Gold Challenge :)

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -10,7 +10,7 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
-  String exchangeRate = '?';
+  List<Widget> coinCardsList = [];
   CoinData coinData = CoinData();
 
   DropdownButton<String> androidDropdown() {
@@ -29,7 +29,7 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
-          getData();
+          getCoinCards(cryptoList);
         });
       },
     );
@@ -47,20 +47,53 @@ class _PriceScreenState extends State<PriceScreen> {
       onSelectedItemChanged: (selectedIndex) {
         print(selectedIndex);
         selectedCurrency = currenciesList[selectedIndex];
-        getData();
+        getCoinCards(cryptoList);
       },
       children: pickerItems,
     );
   }
 
-  void getData() async {
+  void getCoinCards(List<String> coinList) async {
+    List<Widget> holderList = [];
+    for (String coin in coinList) {
+      String price = await getData(coin);
+      holderList.add(
+        Padding(
+          padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+          child: Card(
+            color: Colors.lightBlueAccent,
+            elevation: 5.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+              child: Text(
+                '1 $coin = $price $selectedCurrency',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    setState(() {
+      coinCardsList = holderList;
+    });
+  }
+
+  Future<String> getData(String coin) async {
     try {
       double holder = await coinData.getCoinData(
-          fiatType: selectedCurrency, coinType: 'BTC');
+          fiatType: selectedCurrency, coinType: coin);
+      String price = holder.toStringAsFixed(0);
 
-      setState(() {
-        exchangeRate = holder.toStringAsFixed(0);
-      });
+      return price;
     } catch (e) {
       print(e);
     }
@@ -69,7 +102,7 @@ class _PriceScreenState extends State<PriceScreen> {
   @override
   void initState() {
     super.initState();
-    getData();
+    getCoinCards(cryptoList);
   }
 
   @override
@@ -82,26 +115,11 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $exchangeRate $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: coinCardsList,
           ),
           Container(
             height: 150.0,
