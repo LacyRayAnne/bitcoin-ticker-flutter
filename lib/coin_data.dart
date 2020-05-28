81ebeafc82c3f64d1127842ceedcc097d10b2676
LacyRayAnne <LacyRayAnne@gmail.com>
2020-05-28 14:11:48 -0400

    Added try catch to it after seeing solution and changed some names to match

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,30 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '5CEB0F7A-2C45-4B2D-9008-E3198FCADEA7';
+
+class CoinData {
+  CoinData({this.fiat, this.coin});
+
+  String fiat;
+  String coin;
+
+  Future getCoinData(
+      {@required String fiatType, @required String coinType}) async {
+    double exchangeRate;
+    var coinData;
+    http.Response response =
+        await http.get('$coinAPIURL/$coinType/$fiatType?apikey=$apiKey');
+
+    if (response.statusCode == 200) {
+      coinData = jsonDecode(response.body);
+      exchangeRate = coinData['rate'];
+      return exchangeRate;
+    } else {
+      print(response.statusCode);
+      print(response.body);
+      throw 'problem fetching data Status Code: ${response.statusCode}';
+    }
+  }
+}
